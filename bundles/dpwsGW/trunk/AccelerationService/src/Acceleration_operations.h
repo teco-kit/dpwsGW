8fe573a08e3eafebf78010dc3b6489c84c8c2f37
riedel <riedel@8558c0a7-2cd5-0310-9c60-ed1042e00004>
2011-03-01 16:56:18 +0000

    Reflected changes in GW Fixed missing duration ...

---
+++ bundles/dpwsGW/trunk/AccelerationService/src/Acceleration_operations.h
@@ -0,0 +1,26 @@
+
+#ifndef ACCELERATION_OPERATIONS_H
+#define ACCELERATION_OPERATIONS_H
+
+#define SRV_Acceleration 0
+
+enum Acceleration_operations {
+	OP_GetAccelerationValuesEvent,
+	OP_StartLDC
+};
+
+typedef struct {
+	float values[8];
+	float timertick;
+	float delta;
+} ldcmessage;
+
+typedef struct {
+	char rate[64];
+	char duration[64];
+} LDCInfo;
+
+#define	ACLERR_NotReady -1
+#define ACLERR_GW_Busy -2
+
+#endif //ACCELERATION_OPERATIONS_H
