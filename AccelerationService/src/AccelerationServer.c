b68d0a2045d116db4bf0b723ea47f8bcc4963686
wildschu <5690878+dlwildschut@users.noreply.github.com>
2011-08-29 14:54:28 +0000

    Changes to support SoapFaults

--- AccelerationService/src/AccelerationServer.c
+++ AccelerationService/src/AccelerationServer.c
@@ -40,50 +40,9 @@ static int soap_serve_StartLDC(struct soap *soap) //TODO: pass device context
 
 		soap->omode |= SOAP_IO_CHUNK;
 
-		if (!To || (0 == strcmp(To, wsa_anonymousURI))) {
-			To = wsa_anonymousURI;
-		} else {
+		wsa_response(soap, NULL, To, Action, MessageId,
+				sizeof(struct SOAP_ENV__Header));
 
-		}
-
-		if (To != wsa_anonymousURI) // no strcmp needed -> s.a.
-		{
-			struct soap *reply_soap = soap_copy(soap);
-			if (reply_soap) {
-				soap_copy_stream(reply_soap, soap);
-				soap_clr_omode(reply_soap, SOAP_ENC_MIME | SOAP_ENC_DIME
-						| SOAP_ENC_MTOM);
-				soap->socket = SOAP_INVALID_SOCKET; /* prevents close */
-				if (soap_connect(soap, To, Action)) /*Todo: can this be delayed ??*/
-					return soap->error;
-				soap_send_empty_response(reply_soap, SOAP_OK); /* HTTP ACCEPTED */
-				soap_closesock(reply_soap);
-				soap_end(reply_soap);
-				soap_free(reply_soap);
-				/*
-				 data->fresponse = soap->fresponse;
-				 soap->fresponse = soap_wsa_response;
-				 response will be a POST */
-			} else
-				return soap->error;
-		} else {
-			if (soap_response(soap, SOAP_OK) != SOAP_OK)
-				return soap->error;
-		}
-
-		wsa_header_gen_response(soap, NULL, To, Action, MessageId,
-									sizeof(struct SOAP_ENV__Header));
-		if(error!=WS4D_OK)
-		{
-
-           return error;
-		}
-
-	}
-
-	if (soap_envelope_begin_out(soap) || soap_putheader(soap)
-			|| soap_body_begin_out(soap)) {
-		return soap->error;
 	}
 
 	{
@@ -100,6 +59,13 @@ static int soap_serve_StartLDC(struct soap *soap) //TODO: pass device context
 
 
 	}
+
+	if (soap_response(soap, SOAP_OK) || soap_envelope_begin_out(soap) || soap_putheader(soap)
+			|| soap_body_begin_out(soap)) {
+		return soap->error;
+	}
+
+
 
 	if (soap_body_end_out(soap) || soap_envelope_end_out(soap)
 			|| soap_end_send(soap)) {
